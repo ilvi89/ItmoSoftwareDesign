493763e41c665256a299c29b2732f851095e284d
Ilya Pizik <ilya.pizik89@gmail.com>
2024-05-08 15:58:22 +0300

    refactor

--- src/main/java/ru/abolsoft/sseconnect/core/entity/Member.java
+++
@@ -1,9 +0,0 @@
-package ru.abolsoft.sseconnect.core.entity;
-
-import lombok.Getter;
-import ru.abolsoft.sseconnect.base.entity.BaseEntity;
-
-@Getter
-public class Member extends BaseEntity<Long> {
-    private Long id;
-}
